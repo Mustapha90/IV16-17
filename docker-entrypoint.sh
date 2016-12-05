1b3bc596a9eed0db6b1ef5f6076262c4a0d29694
Mustapha Mayo <mj4ever001@gmail.com>
2016-12-05 22:34:14 +0100

    Mostrar el ip del contenedor al usuario

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 make migrate
-make run_prod
+echo "$(tput setaf 3)IP del contenedor: $(tput setaf 6)$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')$(tput sgr 0)"
