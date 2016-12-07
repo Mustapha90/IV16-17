3b527f52583b70221ac3120aeb9c132a98c4e4f3
Mustapha Mayo <mj4ever001@gmail.com>
2016-12-07 11:01:51 +0100

    docker_entrypoint.sh funciona correctamente closes #25

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -3,7 +3,7 @@
 make migrate
 make populate
 make staticfiles
-echo "$(tput setaf 3)IP del contenedor: $(tput setaf 6)$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')$(tput sgr 0)"
+echo "$(tput setaf 3)IP del contenedor:  $(tput setaf 6)$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')$(tput sgr 0)"
 printf "\nPara Lanzar la aplicación:\nmake docker_run\n"
 /bin/bash
 
